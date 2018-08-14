0df931a1c74535d28f25b4b696c0ce09f060bf25
rogilmore <thatrickgilmore@gmail.com>
2018-08-14 15:48:23 -0400

    Clean-up, move file/dir to Slow R

--- R/Get_bootcamp_googlesheet.R
+++ R/Get_bootcamp_googlesheet.R
@@ -7,13 +7,11 @@ library(tidyverse)
 
 survey_url <- "https://docs.google.com/spreadsheets/d/1-YB0iWUNN_9oxBhz221NFiyBOcwMfHziFeUiUvQwn7k/edit?usp=sharing"
 
-bootcamp_by_url <- survey_url %>%
-  extract_key_from_url() %>%
-  gs_key()
+bootcamp_by_url <- gs_url(survey_url)
 
 bootcamp_sheets <- gs_ws_ls(bootcamp_by_url)
 
 boot_data <- bootcamp_by_url %>%
   gs_read(bootcamp_sheets[1])
           
-write_csv(boot_data, path="data/survey.csv")
+write_csv(boot_data, path="data/raw_survey.csv")
