7a1209901f1970c961e602b8ca647d53db89bd69
rogilmore <thatrickgilmore@gmail.com>
2018-08-14 09:28:13 -0400

    Various changes

--- R/Make_site.R
+++ R/Make_site.R
@@ -1,32 +1,26 @@
 # Make_site.R
 #
 # Updates and makes R bootcamp site
-#
 
-# render ioslides_presentation last because github_document makes HTML
-rmarkdown::render("talks/r-eproducible-science.Rmd", 
-                  output_format = c("github_document", 
-                                    "pdf_document", 
-                                    "word_document", 
-                                    "ioslides_presentation"))
+# Gilmore's talks
+rmarkdown::render(input = "talks/bootcamp-day-1-intro.Rmd", 
+                  output_format = "ioslides_presentation")
+
+rmarkdown::render(input = "talks/slow-r.Rmd", 
+                  output_format = c("html_document"))
+
+rmarkdown::render(input = "talks/r-eproducible-science.Rmd", 
+                  output_format = c("html_document"))
+
 # Survey data
 source("R/Update_survey.R")
 source("R/Make_test_survey.R")
-rmarkdown::render("talks/bootcamp-survey.Rmd",
-                  output_format = c("github_document", 
-                                    "pdf_document", 
-                                    "word_document", 
-                                    "ioslides_presentation"))
+rmarkdown::render(input = "talks/bootcamp-survey.Rmd",
+                  output_format = c("pdf_document", 
+                                    "html_document"))
 
 # Sample papja document
-rmarkdown::render("papaja_demo/gilmore-lebreton-hallquist.Rmd")
-
-# James talk
-rmarkdown::render("talks/R-Workshop-James.Rmd",
-                  output_format = c("github_document",
-                                    "pdf_document",
-                                    "word_document",
-                                    "ioslides_presentation"))
+rmarkdown::render(input = "talks/gilmore-hallquist-bootcamp-2018-papaja.Rmd")
 
 # Render site last so that updated versions get copied to docs/
 rmarkdown::render_site()
