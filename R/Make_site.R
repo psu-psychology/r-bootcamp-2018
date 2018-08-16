dc9b629712a7abd311f21eb8a3ec2304b8f5a03e
rogilmore <thatrickgilmore@gmail.com>
2018-08-16 05:35:04 -0400

    Edit, test, & run Make_site

--- R/Make_site.R
+++ R/Make_site.R
@@ -2,6 +2,12 @@
 #
 # Updates and makes R bootcamp site
 
+# Survey data (update first because later items depend on it)
+source("R/Make_test_survey.R")
+source("R/Get_bootcamp_googlesheet.R")
+rmarkdown::render("talks/bootcamp-survey.Rmd", 
+                  output_format = "html_document")
+
 # Gilmore's talks
 rmarkdown::render(input = "talks/bootcamp-day-1-intro.Rmd", 
                   output_format = "ioslides_presentation")
@@ -11,13 +17,6 @@ rmarkdown::render(input = "talks/slow-r.Rmd",
 
 rmarkdown::render(input = "talks/r-eproducible-science.Rmd", 
                   output_format = c("html_document"))
-
-# Survey data
-source("R/Update_survey.R")
-source("R/Make_test_survey.R")
-rmarkdown::render(input = "talks/bootcamp-survey.Rmd",
-                  output_format = c("pdf_document", 
-                                    "html_document"))
 
 # Sample papja document
 rmarkdown::render(input = "talks/gilmore-hallquist-bootcamp-2018-papaja.Rmd")
