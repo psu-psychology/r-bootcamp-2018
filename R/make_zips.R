dfa2664bfdf54e064f591fcde46f3b5b1bb80917
Michael Hallquist <michael.hallquist@gmail.com>
2018-08-16 12:59:46 -0400

    zip for parallel

--- R/make_zips.R
+++ R/make_zips.R
@@ -5,3 +5,9 @@ system(paste("zip zips/ram_efa.zip", paste(ram_efa, collapse=" ")))
 
 hallquist_lavaan <- c("talks/lavaan_tutorial.html", "talks/lavaan_tutorial.Rmd", "img/threshold_structure.jpg")
 system(paste("zip zips/hallquist_lavaan.zip", paste(hallquist_lavaan, collapse=" ")))
+
+hallquist_parallel <- c("talks/parallel_r.html", "talks/parallel_r.Rmd", "img/few_bottles.jpg",
+                        "img/split-apply-combine.png", "img/fork.png", "img/levy_geospatial.png",
+                        "img/wine_cellar.jpg")
+system(paste("zip zips/hallquist_parallel.zip", paste(hallquist_parallel, collapse=" ")))
+
